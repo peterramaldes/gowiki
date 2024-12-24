d34cdf52bad9235f6f9e0c31364d1a62446b7fc6
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-12-24 15:34:35 -0300

    Created page package
    Beside the page package being created the editHandler was also created.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/peterramaldes/gowiki
+
+go 1.23.4
--- wiki.go
+++ wiki.go
@@ -6,33 +6,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+
+	"github.com/peterramaldes/gowiki/internal/page"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := page.LoadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+
+	p, err := page.LoadPage(title)
+	if err != nil {
+		p = &page.Page{Title: title}
+	}
+
+	body := `
+	<h1>Editing %s</h1>
+	<form action="/save/%s" method="POST">
+		<textarea name="body">%s</textarea>
+		</br>
+		<input type="submit" value="Save">
+	</form>
+	`
+
+	fmt.Fprintf(w, body, p.Title, p.Title, p.Body)
 }
